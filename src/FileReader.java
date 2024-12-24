79a73c4ed79358caa3829e678e09f35be7894c61
Влад Кузьмин <vladkuzmin997@gmail.com>
2024-12-24 17:00:21 +0400

    added FileReader, changed classes

--- src/FileReader.java
+++ src/FileReader.java
@@ -5,16 +5,52 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class FileReader {
-    public ArrayList<Book> ReadBooks(String fileName) throws IOException, NumberFormatException {
-        ArrayList<Book> books = new ArrayList<>();
+    public ArrayList<Human> ReadHumans(String fileName) throws IOException, NumberFormatException {
+        ArrayList<Human> humans = new ArrayList<>();
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 var items = line.split(" ");
-                books.add(new Book(items[0], Integer.parseInt(items[1]), Integer.parseInt(items[2])));
+                humans.add(new Human.HumanBuilder()
+                        .setSurname(items[0])
+                        .setAge(Integer.parseInt(items[1]))
+                        .setGender(items[2])
+                        .build());
             }
         }
-        return books;
+        return humans;
+    }
+
+    public ArrayList<Barrel> ReadBarrels(String fileName) throws IOException, NumberFormatException {
+        ArrayList<Barrel> barrels = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                var items = line.split(" ");
+                barrels.add(new Barrel.BarrelBuilder()
+                        .setVolume(Double.parseDouble(items[0]))
+                        .setMaterial(items[1])
+                        .setContent(items[2])
+                        .build());
+            }
+        }
+        return barrels;
+    }
+
+    public ArrayList<Animal> ReadAnimals(String fileName) throws IOException, NumberFormatException {
+        ArrayList<Animal> animals = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                var items = line.split(" ");
+                animals.add(new Animal.AnimalBuilder()
+                        .setType(items[0])
+                        .setEyeColor(items[1])
+                        .setHasFur(Boolean.parseBoolean(items[2]))
+                        .build());
+            }
+        }
+        return animals;
     }
 }
 
